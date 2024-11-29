730199059b49eba2b51d45de6c2447ed864e51c2
KAMRJ <laurita.revelo@udla.edu.ec>
2024-11-29 15:48:36 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KRMauiAppApuntes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
